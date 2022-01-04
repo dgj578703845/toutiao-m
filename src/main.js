ff7d14901acd651137319d75b06699fe28279a31
dgj578703845 <578703845@qq.com>
2022-01-04 09:18:34 +0800

    项目初始化引入vant组件

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import store from './store'
 import './styles/index.css'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 Vue.config.productionTip = false
 new Vue({
   router,
